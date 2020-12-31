c4d66ab2288f40f99b32064f267e3be378329d7b
KRozumniak <kirkterra@gmail.com>
2020-12-30 23:18:02 -0500

    Some updates

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb+srv://KirkRk:' + process.env.MONGO_ATLAS_PW +'@cluster0.3gal1.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+)
+
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
@@ -16,12 +25,11 @@ app.use((req, res, next) => {
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
   if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET', );
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET',);
     return res.status(200).json({});
   }
   next();
 });
-
 
 // Routes which should handle requests
 app.use('/products', productRoutes);
